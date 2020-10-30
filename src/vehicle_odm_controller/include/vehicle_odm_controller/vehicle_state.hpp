4288bcfe3b127883e8b9d9ebb4df71dbf2f23f92
TakayukiKanai <kanai@isi.imi.i.u-tokyo.ac.jp>
2020-10-30 15:43:48 +0000

    Listen odom msgs

--- src/vehicle_odm_controller/include/vehicle_odm_controller/vehicle_state.hpp
+++ src/vehicle_odm_controller/include/vehicle_odm_controller/vehicle_state.hpp
@@ -4,6 +4,7 @@
 // #include <sstream>
 
 // ROS packages
+#include <iostream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sensor_msgs/Imu.h>
@@ -25,16 +26,10 @@ public:
 
 private:
   // Not ROS specific func.
-    double calcDiffForRadian(const double lhs_rad, const double rhs_rad);
-    double wrapToPm(double a_num, const double a_max);
-    double wrapToPmPi(double a_angle_rad);
     void init_varibles();
 
   // Callback and the method of which
-    void odm_callback(const sensor_msgs::Imu::Ptr& input);
-    void calc_vel_from_imu(ros::Time current_time);
-    void set_twist_stamped();
-    void vel_pub_timer(const ros::TimerEvent& e);
+    void odm_callback(const nav_msgs::Odometry::Ptr& input);
 
     ros::NodeHandle nh;
     ros::Subscriber imu_sub_;
@@ -43,16 +38,8 @@ private:
 
     nav_msgs::Odometry odom;
     ros::Time previous_time;
-    double current_velocity_imu_x;
-    double current_velocity_imu_y;
-    double current_velocity_imu_z;
-    double current_angular_velocity_imu_x;
-    double current_angular_velocity_imu_y;
-    double current_angular_velocity_imu_z;
-    double previous_imu_roll;
-    double previous_imu_pitch;
-    double previous_imu_yaw;
-    double offset_imu_x, offset_imu_y, offset_imu_z, offset_imu_roll, offset_imu_pitch, offset_imu_yaw;
+    double vx, vth;
+
 };
 
 #endif //TEST_VEHICLE_ODM_CONTROLLER_H
--- src/vehicle_odm_controller/src/vehicle_state.cpp
+++ src/vehicle_odm_controller/src/vehicle_state.cpp
@@ -1,30 +1,16 @@
 #include "vehicle_state.hpp"
 #define __APP_NAME__ "vehicle_odm_controller"
 
-// using namespace vehicle_odm_controller;
-
 VehicleOdmController::VehicleOdmController(){
   init_varibles();
 }
 
 void VehicleOdmController::init_varibles(){
-  current_velocity_imu_x = 0.0;
-  current_velocity_imu_y = 0.0;
-  current_velocity_imu_z = 0.0;
-  current_angular_velocity_imu_x = 0.0;
-  current_angular_velocity_imu_y = 0.0;
-  current_angular_velocity_imu_z = 0.0;
-  offset_imu_x = 0.0;
-  offset_imu_y = 0.0;
-  offset_imu_z = 0.0;
-  offset_imu_roll = 0.0;
-  offset_imu_pitch = 0.0;
-  offset_imu_yaw = 0.0;
-  previous_imu_roll = 0.0;
   previous_time =  ros::Time::now();
-  imu_sub_ = nh.subscribe("odm", 10, &VehicleOdmController::odm_callback, this);
-  f_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("fast_current_vel", 10);
-  // ros::Timer timer = nh.createTimer(ros::Duration(0.1), VehicleOdmController::vel_pub_timer);
+  vx = 0;
+  vth = 0;
+  imu_sub_ = nh.subscribe("odom", 10, &VehicleOdmController::odm_callback, this);
+  // f_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("fast_current_vel", 10);
 }
 
 void VehicleOdmController::odm_callback(const nav_msgs::Odometry::Ptr& input)
@@ -33,150 +19,18 @@ void VehicleOdmController::odm_callback(const nav_msgs::Odometry::Ptr& input)
   const ros::Time current_time = input->header.stamp;
   const double diff_time = (current_time - previous_time).toSec();
 
-  double imu_roll, imu_pitch, imu_yaw;
-  tf::Quaternion imu_orientation;
-  tf::quaternionMsgToTF(input->orientation, imu_orientation);
-  tf::Matrix3x3(imu_orientation).getRPY(imu_roll, imu_pitch, imu_yaw);
-
-  imu_roll = wrapToPmPi(imu_roll);
-  imu_pitch = wrapToPmPi(imu_pitch);
-  imu_yaw = wrapToPmPi(imu_yaw);
-
-  // static double previous_imu_roll = imu_roll, previous_imu_pitch = imu_pitch, previous_imu_yaw = imu_yaw;
-  const double diff_imu_roll = calcDiffForRadian(imu_roll, previous_imu_roll);
-  const double diff_imu_pitch = calcDiffForRadian(imu_pitch, previous_imu_pitch);
-  const double diff_imu_yaw = calcDiffForRadian(imu_yaw, previous_imu_yaw);
-
-  imu.header = input->header;
-  imu.linear_acceleration.x = input->linear_acceleration.x;
-  // imu.linear_acceleration.y = input->linear_acceleration.y;
-  // imu.linear_acceleration.z = input->linear_acceleration.z;
-  imu.linear_acceleration.y = 0;
-  imu.linear_acceleration.z = 0;
-
-  if (diff_time != 0)
-  {
-    ROS_INFO("here");
-    imu.angular_velocity.x = diff_imu_roll / diff_time;
-    imu.angular_velocity.y = diff_imu_pitch / diff_time;
-    imu.angular_velocity.z = diff_imu_yaw / diff_time;
-    current_angular_velocity_imu_x = diff_imu_roll / diff_time;
-    current_angular_velocity_imu_y = diff_imu_pitch / diff_time;
-    current_angular_velocity_imu_z = diff_imu_yaw / diff_time;
-
-  }
-  else
-  {
-    current_angular_velocity_imu_x = 0;
-    current_angular_velocity_imu_y = 0;
-    current_angular_velocity_imu_z = 0;
-    // ROS_INFO("NOT UPDATED");
-    imu.angular_velocity.x = 0;
-    imu.angular_velocity.y = 0;
-    imu.angular_velocity.z = 0;
-  }
-
-  calc_vel_from_imu(input->header.stamp);
-
+  // read velocity
+  vx =  input -> twist.twist.linear.x;
+  std::cout << vx << "\n";
   previous_time = current_time;
-  previous_imu_roll = imu_roll;
-  previous_imu_pitch = imu_pitch;
-  previous_imu_yaw = imu_yaw;
 }
-
-double VehicleOdmController::wrapToPm(double a_num, const double a_max)
-{
-  if (a_num >= a_max)
-  {
-    a_num -= 2.0 * a_max;
-  }
-  return a_num;
-}
-
-double VehicleOdmController::wrapToPmPi(double a_angle_rad)
-{
-  return wrapToPm(a_angle_rad, M_PI);
-}
-
-double VehicleOdmController::calcDiffForRadian(const double lhs_rad, const double rhs_rad)
-{
-  double diff_rad = lhs_rad - rhs_rad;
-  if (diff_rad >= M_PI)
-    diff_rad = diff_rad - 2 * M_PI;
-  else if (diff_rad < -M_PI)
-    diff_rad = diff_rad + 2 * M_PI;
-  return diff_rad;
-}
-
-void VehicleOdmController::calc_vel_from_imu(ros::Time current_time)
-{
-  double diff_time = (current_time - previous_time).toSec();
-  // std::cout << "time " << diff_time << "\n";
-
-  double diff_imu_roll = imu.angular_velocity.x * diff_time;
-  double diff_imu_pitch = imu.angular_velocity.y * diff_time;
-  double diff_imu_yaw = imu.angular_velocity.z * diff_time;
-
-  current_pose_imu.roll += diff_imu_roll;
-  current_pose_imu.pitch += diff_imu_pitch;
-  current_pose_imu.yaw += diff_imu_yaw;
-
-  double accX1 = imu.linear_acceleration.x;
-  double accY1 = std::cos(current_pose_imu.roll) * imu.linear_acceleration.y -
-                 std::sin(current_pose_imu.roll) * imu.linear_acceleration.z;
-  double accZ1 = std::sin(current_pose_imu.roll) * imu.linear_acceleration.y +
-                 std::cos(current_pose_imu.roll) * imu.linear_acceleration.z;
-
-  double accX2 = std::sin(current_pose_imu.pitch) * accZ1 + std::cos(current_pose_imu.pitch) * accX1;
-  double accY2 = accY1;
-  double accZ2 = std::cos(current_pose_imu.pitch) * accZ1 - std::sin(current_pose_imu.pitch) * accX1;
-
-  double accX = std::cos(current_pose_imu.yaw) * accX2 - std::sin(current_pose_imu.yaw) * accY2;
-  double accY = std::sin(current_pose_imu.yaw) * accX2 + std::cos(current_pose_imu.yaw) * accY2;
-  double accZ = accZ2;
-
-  // offset_imu_x += current_velocity_imu_x * diff_time + accX * diff_time * diff_time / 2.0;
-  // offset_imu_y += current_velocity_imu_y * diff_time + accY * diff_time * diff_time / 2.0;
-  // offset_imu_z += current_velocity_imu_z * diff_time + accZ * diff_time * diff_time / 2.0;
-
-  current_velocity_imu_x += accX * diff_time;
-  current_velocity_imu_y += accY * diff_time;
-  current_velocity_imu_z += accZ * diff_time;
-
-  offset_imu_roll += diff_imu_roll;
-  offset_imu_pitch += diff_imu_pitch;
-  offset_imu_yaw += diff_imu_yaw;
-
-  // predict_pose_imu.x = previous_pose.x + offset_imu_x;
-  // predict_pose_imu.y = previous_pose.y + offset_imu_y;
-  // predict_pose_imu.z = previous_pose.z + offset_imu_z;
-  // predict_pose_imu.roll = previous_pose.roll + offset_imu_roll;
-  // predict_pose_imu.pitch = previous_pose.pitch + offset_imu_pitch;
-  // predict_pose_imu.yaw = previous_pose.yaw + offset_imu_yaw;
-
-  // previous_time = current_time;
-}
-
-void VehicleOdmController::set_twist_stamped(){
-  twist_.header.stamp = ros::Time::now();
-  twist_.twist.linear.x = current_velocity_imu_x;
-  twist_.twist.linear.y = current_velocity_imu_y;
-  twist_.twist.linear.z = current_velocity_imu_z;
-  twist_.twist.angular.x = current_angular_velocity_imu_x;
-  twist_.twist.angular.y = current_angular_velocity_imu_y;
-  twist_.twist.angular.z = current_angular_velocity_imu_z;
-}
-
-void VehicleOdmController::vel_pub_timer(const ros::TimerEvent& e){}
 
 void VehicleOdmController::run() {
   ros::Rate loop_rate(30);
   while (ros::ok())
   {
-    set_twist_stamped();
-    f_vel_pub.publish(twist_);
+    std::cout << vx << "\n";
     ros::spinOnce();
     loop_rate.sleep();
   }
-  // ros::spin();
 }
