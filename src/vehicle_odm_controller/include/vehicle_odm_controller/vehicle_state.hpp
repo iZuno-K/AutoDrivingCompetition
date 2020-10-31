d00c450d4902080eb84b1fff2e1c6fccf7bfae73
TakayukiKanai <kanai@isi.imi.i.u-tokyo.ac.jp>
2020-10-31 10:46:27 +0000

    cannot read VehicleCmd

--- src/vehicle_odm_controller/include/vehicle_odm_controller/vehicle_state.hpp
+++ src/vehicle_odm_controller/include/vehicle_odm_controller/vehicle_state.hpp
@@ -15,6 +15,9 @@
 #include <autoware_msgs/ControlCommandStamped.h> 
 #include <autoware_msgs/VehicleCmd.h> // publish acclel command
 
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/TwistStamped.h>
@@ -27,6 +30,12 @@ public:
     void run();
 
 private:
+  // Def synclonizers
+  typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry, autoware_msgs::VehicleCmd> SyncPolicyT;
+  message_filters::Synchronizer<SyncPolicyT> *synchronizer_;
+  message_filters::Subscriber<nav_msgs::Odometry> *sub_odom_;
+  message_filters::Subscriber<autoware_msgs::VehicleCmd> *sub_vcmd_;
+
   // Not ROS specific func.
     void init_odom();
     void init_vcd();
@@ -39,11 +48,9 @@ private:
     ros::Subscriber odm_sub, vehicle_cmd_sub;
     ros::Publisher f_vel_pub;
 
-    nav_msgs::Odometry odom;
     ros::Time previous_time;
-    double vx, pre_vx, vth, ax, pre_ax;
-    autoware_msgs::VehicleCmd vehicle_cmd_msg_;
-
+    double vx_odm, vx_odm_prev, ax_odm, ax_odm_prev;
+    double vx_cmd, vx_cmd_prev, ax_cmd, ax_cmd_prev;
 };
 
 #endif //TEST_VEHICLE_ODM_CONTROLLER_H
--- src/vehicle_odm_controller/src/vehicle_state.cpp
+++ src/vehicle_odm_controller/src/vehicle_state.cpp
@@ -7,41 +7,61 @@ VehicleOdmController::VehicleOdmController(){
 
 void VehicleOdmController::init_odom(){
   previous_time =  ros::Time::now();
-  vx = 0;
-  pre_vx = 0;
-  vth = 0;
-  ax = 0;
-  pre_ax = 0;
+  vx_odm = 0;
+  vx_odm_prev = 0;
+  ax_odm = 0;
+  ax_odm_prev = 0;
   odm_sub = nh.subscribe("odom", 10, &VehicleOdmController::odm_callback, this);
-  vehicle_cmd_sub = nh.subscribe("vehicle_cmd", 10, &VehicleOdmController::vehicle_cmd_callback, this);
 }
 
 void VehicleOdmController::odm_callback(const nav_msgs::Odometry::Ptr& input)
 {
   const ros::Time current_time = input->header.stamp;
-  vx =  input -> twist.twist.linear.x;
-
+  vx_odm =  input -> twist.twist.linear.x;
   const double diff_time = (current_time - previous_time).toSec();
   if (diff_time != 0){
-    ax = (vx - pre_vx)/ diff_time;
+    ax_odm = (vx_odm - vx_odm_prev)/ diff_time;
   } else {
-    ax = pre_ax;
+    ax_odm = ax_odm_prev;
   }
   previous_time = current_time;
-  pre_vx = vx;
-  pre_ax = ax;
+  vx_odm_prev = vx_odm;
+  ax_odm_prev = ax_odm;
 }
 
 void VehicleOdmController::vehicle_cmd_callback(const autoware_msgs::VehicleCmd::Ptr& input){
-
+  // const ros::Time current_time = input->header.stamp;
+  vx_cmd = input->ctrl_cmd.linear_velocity;
+  ax_cmd = input->ctrl_cmd.linear_acceleration;
 }
 
 void VehicleOdmController::run() {
   ros::Rate loop_rate(10);
   while (ros::ok())
   {
-    std::cout << vx << "\t" << ax << "\n";
+    std::cout << vx_odm << "\t" << ax_odm << "\t";
+    std::cout << vx_cmd << "\t" << ax_cmd << "\n";
     ros::spinOnce();
     loop_rate.sleep();
   }
 }
+
+
+// #include <nav_msgs/Odometry.h>
+// #include <message_filters/subscriber.h>
+// #include <message_filters/synchronizer.h>
+// #include <message_filters/sync_policies/approximate_time.h>
+// #include <autoware_msgs/DetectedObjectArray.h>
+// #include <autoware_msgs/VehicleCmd.h>
+// typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry, autoware_msgs::VehicleCmd> SyncPolicyT;
+// message_filters::Subscriber<nav_msgs::Odometry> *sub_odom_;
+// message_filters::Subscriber<autoware_msgs::VehicleCmd> *sub_vcmd_;
+// message_filters::Synchronizer<SyncPolicyT> *synchronizer_;
+// void callback(const nav_msgs::Odometry::ConstPtr &in_1,
+//                   const autoware_msgs::DetectedObjectArray::ConstPtr &in_2);
+// void init() {
+//     sub_odom_ = new message_filters::Subscriber<autoware_msgs::DetectedObjectArray>(nh, topic1_, 1);
+//     sub_vcmd_ = new message_filters::Subscriber<autoware_msgs::DetectedObjectArray>(nh, topic2_, 1);
+//     synchronizer_ = new message_filters::Synchronizer<SyncPolicyT>(SyncPolicyT(10), *sub_odom_, *sub_vcmd_);
+// 	synchronizer_->registerCallback(boost::bind(&CLASS::callback, this, _1, _2));
+// }
